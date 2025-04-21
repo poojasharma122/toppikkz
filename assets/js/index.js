f8bceb9732de6ea8cb7d1f4829135c647b9adc84
komalverma06 <komalverma1906@gmail.com>
2025-04-21 17:45:08 +0530

    change

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,45 +6,36 @@ $(document).ready(function () {
   $(".menu-close-btn").click(function () {
     $("body").removeClass("menuToggle");
   });
-});  
+});
 // Toggle Menu Functionaliy End
 
 // AOS JS Start
 AOS.init({
-  duration:1200,
+  duration: 1200,
 });
 // AOS JS End
 
 // Slick Slider JS Start
- $('.home_featured_slider').slick({
+$('.home_featured_slider').slick({
   centerMode: true,
   centerPadding: '60px',
   slidesToShow: 2,
   slidesToScroll: 1,
-  infinite:true,
-  autoplay:true,
-  autoplaySpeed:1500,
-  dots:true,
-  arrow:true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  dots: true,
+  arrow: true,
   responsive: [
     {
       breakpoint: 768,
       settings: {
         arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 2
+        centerMode: false,
+        centerPadding: '0',
+        slidesToShow: 1,
       }
     },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: '40px',
-        slidesToShow: 1
-      }
-    }
   ]
 });
 // Slick Slider JS End
